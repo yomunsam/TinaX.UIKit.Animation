063eaea8b4c312dc0f301dab7980f1c12ca6c404
yomunsam <yomunsam@outlook.com>
2021-02-28 22:20:20 +0800

    阶段性的保存一波

---
+++ Runtime/Scripts/Const/UIAniConst.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinaX.UIKit.Animation.Const
+{
+    public static class UIAniConst
+    {
+        public const string ComponentMenuBasePath = @"TinaX/UGUI Tween/";
+    }
+}
